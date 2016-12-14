1cef1cfee0e5755ad23166b7787451a7036b1b32
Oystein Amundsen <oystein.amundsen@bouvet.no>
2016-12-14 09:33:16 +0100

    Fixed angular routing in spring

--- fint-admin-portal-frontend/client/app/views/organization/organization.routes.ts
+++ fint-admin-portal-frontend/client/app/views/organization/organization.routes.ts
@@ -5,7 +5,7 @@ import { EditOrganizationComponent } from './edit-organization/edit-organization
 
 export const OrganizationRoutes: Routes = [
   {
-    path: 'organizations', data: { label: 'Organisasjoner', icon: 'sitemap' }, children: [
+    path: 'organisations', data: { label: 'Organisasjoner', icon: 'sitemap' }, children: [
       { path: '', component: OrganizationComponent, pathMatch: 'full' },
       { path: 'add', component: EditOrganizationComponent },
       { path: ':orgId', component: EditOrganizationComponent },
