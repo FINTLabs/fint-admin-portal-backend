d921d28e179f290f6f7735911a4750c8947f9907
Oystein Amundsen <oystein.amundsen@bouvet.no>
2017-02-16 12:56:39 +0100

    Upgraded angular material

--- fint-admin-portal-frontend/upgrade-cli.cmd
+++ fint-admin-portal-frontend/upgrade-cli.cmd
@@ -1,4 +1,4 @@
 rm -rf node_modules dist tmp
 cmd /C npm i angular-cli@latest -D
 cmd /C npm install
-ng init --source-dir=client --style=scss --routing=true
+ng update --source-dir=client --style=scss --routing=true
