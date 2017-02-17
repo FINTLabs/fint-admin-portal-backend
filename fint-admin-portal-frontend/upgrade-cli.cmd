af6d649648eed77e82fdcaa27ab2b1fda4341745
Oystein Amundsen <oystein.amundsen@bouvet.no>
2017-02-17 10:28:31 +0100

    Upgraded angular-cli

--- fint-admin-portal-frontend/upgrade-cli.cmd
+++ fint-admin-portal-frontend/upgrade-cli.cmd
@@ -1,4 +1,5 @@
 rm -rf node_modules dist tmp
-cmd /C npm i angular-cli@latest -D
+cmd /C npm i @angular/cli@latest -D
 cmd /C npm install
-ng update --source-dir=client --style=scss --routing=true
+
+rem ng update --source-dir=client --style=scss --routing=true
