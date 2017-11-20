a3dcda33566615c02b4a49ce81cd61490b32ef19
Øystein Amundsen <oystein.amundsen@bouvet.no>
2017-11-20 11:00:15 +0100

    Fixing imports

--- client/client/app/views/component/common-component.service.ts
+++ client/client/app/views/component/common-component.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 
-import { Observable } from 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 import { FintDialogService } from 'fint-shared-components';
 
