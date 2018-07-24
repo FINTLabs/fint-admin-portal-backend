ff301726a1848fb2f1a5d354460ae7f6115c4dd1
Frode Sjovatsen <frode@fintprosjektet.no>
2018-07-24 22:52:41 +0200

    added credentials option to fetch

--- client/src/data/api/ComponentApi.js
+++ client/src/data/api/ComponentApi.js
@@ -2,77 +2,90 @@ import {apiUrl} from "./apiUrl";
 
 class ComponentApi {
 
-    static fetchComponents() {
-        const url = apiUrl + `/api/components`;
-        return fetch(url, {method: 'GET'}).then(response => {
-                return response.json();
-            }).catch(error => {
-                return error;
-            })
-    }
+  static fetchComponents() {
+    const url = apiUrl + `/api/components`;
+    return fetch(url,
+      {
+        method: 'GET',
+        credentials: 'same-origin'
+      },
+    ).then(response => {
+      return response.json();
+    }).catch(error => {
+      return error;
+    })
+  }
 
-    static getComponents() {
-        const url = apiUrl + `/api/components`;
-        return fetch(url, {method: 'GET'})
-            .then(response => Promise.all([response, response.json()]));
-    }
+  static getComponents() {
+    const url = apiUrl + `/api/components`;
+    return fetch(url, {
+      method: 'GET',
+      credentials: 'same-origin'
+    })
+      .then(response => Promise.all([response, response.json()]));
+  }
 
-    static createComponent(component) {
-        const url = apiUrl + `/api/components`;
-        const request = new Request(url, {
-            method: 'POST',
-            headers: {
-                'Accept': '*/*',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                dn: component.dn,
-                name: component.name,
-                description: component.description,
-                basePath: component.basePath
-            })
-        });
+  static createComponent(component) {
+    const url = apiUrl + `/api/components`;
+    const request = new Request(url, {
+      method: 'POST',
+      headers: {
+        'Accept': '*/*',
+        'Content-Type': 'application/json'
+      },
+      credentials: 'same-origin',
+      body: JSON.stringify({
+        dn: component.dn,
+        name: component.name,
+        description: component.description,
+        basePath: component.basePath
+      })
+    });
 
-        return fetch(request).then(response => {
-            return response;
-        }).catch(error => {
-            return error;
-        });
-    }
+    return fetch(request).then(response => {
+      return response;
+    }).catch(error => {
+      return error;
+    });
+  }
 
-    static updateComponent(component) {
-        const url = apiUrl + `/api/components/${component.name}`;
-        const request = new Request(url, {
-            method: 'PUT',
-            headers: {
-                'Accept': '*/*',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                dn: component.dn,
-                name: component.name,
-                description: component.description,
-                basePath: component.basePath,
-            })
-        });
+  static updateComponent(component) {
+    const url = apiUrl + `/api/components/${component.name}`;
+    const request = new Request(url, {
+      method: 'PUT',
+      headers: {
+        'Accept': '*/*',
+        'Content-Type': 'application/json'
+      },
+      credentials: 'same-origin',
+      body: JSON.stringify({
+        dn: component.dn,
+        name: component.name,
+        description: component.description,
+        basePath: component.basePath,
+      })
+    });
 
-        return fetch(request).then(response => {
-            return response;
-        }).catch(error => {
-            return error;
-        });
-    }
+    return fetch(request).then(response => {
+      return response;
+    }).catch(error => {
+      return error;
+    });
+  }
 
-    static deleteComponent(component) {
-        const url = apiUrl + `/api/components/${component.name}`;
-        const request = new Request(url, {method: 'DELETE'});
+  static deleteComponent(component) {
+    const url = apiUrl + `/api/components/${component.name}`;
+    const request = new Request(url, {
+      method: 'DELETE',
+      credentials: 'same-origin'
+    });
 
-        return fetch(request).then(response => {
-            return response;
-        }).catch(error => {
-            return error;
-        });
-    }
+    return fetch(request).then(response => {
+      return response;
+    }).catch(error => {
+      return error;
+    });
+  }
 }
 
 export default ComponentApi
--- client/src/data/api/ContactApi.js
+++ client/src/data/api/ContactApi.js
@@ -2,70 +2,81 @@ import {apiUrl} from "./apiUrl";
 
 class ContactApi {
 
-    static fetchContacts() {
-        const url = apiUrl + `/api/contacts`;
-        const request = new Request(url, {method:'GET'});
-        return fetch(request).then(response => {
-            return response.json();
-        }).catch(error => {
-            return error;
-        });
-    }
+  static fetchContacts() {
+    const url = apiUrl + `/api/contacts`;
+    const request = new Request(url, {
+      method: 'GET',
+      credentials: 'same-origin'
+    });
+    return fetch(request).then(response => {
+      return response.json();
+    }).catch(error => {
+      return error;
+    });
+  }
 
-    static getContacts() {
-        const url = apiUrl + `/api/contacts`;
-        return fetch(url, {method: 'GET'})
-            .then(response => Promise.all([response, response.json()]));
-    }
+  static getContacts() {
+    const url = apiUrl + `/api/contacts`;
+    return fetch(url, {
+      method: 'GET',
+      credentials: 'same-origin'
+    })
+      .then(response => Promise.all([response, response.json()]));
+  }
 
-    static createContact(contact) {
-        const url = apiUrl + `/api/contacts`;
-        const request = new Request(url, {
-            method: 'POST',
-            headers: {
-                'Accept': '*/*',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                nin: contact.nin,
-                firstName: contact.firstName,
-                lastName: contact.lastName,
-                mail: contact.mail,
-                mobile: contact.mobile
-            })
-        });
+  static createContact(contact) {
+    const url = apiUrl + `/api/contacts`;
+    const request = new Request(url, {
+      method: 'POST',
+      headers: {
+        'Accept': '*/*',
+        'Content-Type': 'application/json'
+      },
+      credentials: 'same-origin',
+      body: JSON.stringify({
+        nin: contact.nin,
+        firstName: contact.firstName,
+        lastName: contact.lastName,
+        mail: contact.mail,
+        mobile: contact.mobile
+      })
+    });
 
-        return fetch(request).then(response => {
-            return response;
-        }).catch(error => {
-            return error;
-        });
-    }
+    return fetch(request).then(response => {
+      return response;
+    }).catch(error => {
+      return error;
+    });
+  }
 
-    static updateContact(contact) {
-        const request = new Request(apiUrl + `/api/contacts/${contact.nin}`, {
-            method: 'PUT',
-            headers: new Headers({
-                'Content-Type': 'application/json'
-            }),
-            body: JSON.stringify(contact)
-        });
+  static updateContact(contact) {
+    const request = new Request(apiUrl + `/api/contacts/${contact.nin}`, {
+      method: 'PUT',
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      }),
+      credentials: 'same-origin',
+      body: JSON.stringify(contact)
+    });
 
-        return fetch(request).then(response => {
-            return response;
-        }).catch(error => {
-            return error;
-        });
-    }
+    return fetch(request).then(response => {
+      return response;
+    }).catch(error => {
+      return error;
+    });
+  }
 
-    static deleteContact(contact) {
-        const request = new Request(apiUrl + `/api/contacts/${contact.nin}`, {method: 'DELETE'});
-        return fetch(request).then(response => {
-            return response;
-        }).catch(error => {
-            return error;
-        });
-    }
+  static deleteContact(contact) {
+    const request = new Request(apiUrl + `/api/contacts/${contact.nin}`, {
+      method: 'DELETE',
+      credentials: 'same-origin'
+    });
+    return fetch(request).then(response => {
+      return response;
+    }).catch(error => {
+      return error;
+    });
+  }
 }
 
 export default ContactApi;
