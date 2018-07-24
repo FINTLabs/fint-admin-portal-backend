c7e9788bd134cf4a7b1d905302ddb2cb89130c51
Frode Sjovatsen <frode@fintprosjektet.no>
2018-07-24 21:34:40 +0200

    removed registerServiceWorker

--- client/src/index.js
+++ client/src/index.js
@@ -2,17 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import {Provider} from "react-redux";
 import {BrowserRouter} from "react-router-dom";
 import store from './data/redux/store/configure-store';
 
 ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
 );
-registerServiceWorker();
+//registerServiceWorker();
