4df900e197e66b8a97718c43bba22bd6ed6ef6e9
Frode <frode@sjovatsen.net>
2019-11-14 20:38:03 +0100

    Added endpoint for verifying component policies

--- src/main/java/no/fint/portal/Application.java
+++ src/main/java/no/fint/portal/Application.java
@@ -16,7 +16,7 @@ import java.util.Properties;
 @EnableHalHypermediaSupport
 @EnableSpringfox
 @SpringBootApplication
-@EnableAutoConfiguration
+//@EnableAutoConfiguration
 @EnableRetry
 public class Application {
   public static void main(String[] args) {
